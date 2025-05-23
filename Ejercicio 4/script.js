83dd2e4bece2cad0c2b8f9543d26ff61911e7c7e
Joaquin Mario Gomez <joaggomez@gmail.com>
2025-05-23 18:06:38 -0300

    Agrego validacion a peso y altura

--- Ejercicio 4/script.js
+++ Ejercicio 4/script.js
@@ -1,6 +1,11 @@
 function calcularIMC() {
 let peso = parseFloat(document.getElementById("peso").value);
 let altura = parseFloat(document.getElementById("altura").value);
-let imc = peso / (altura * altura);
+if (peso>0 && altura > 0){
+    let imc = peso / (altura * altura);
 alert("Tu IMC es: " + imc.toFixed(2));
+}
+ else{
+    alert("Ingrese valores que sean validos");
+ }
 }
