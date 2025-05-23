53a8c6d38a7122e4883ace01a1b7577b11e04df2
Joaquin Mario Gomez <joaggomez@gmail.com>
2025-05-23 18:50:45 -0300

    correxion del codigo

--- Ejercicio 5/script.js
+++ Ejercicio 5/script.js
@@ -1,7 +1,16 @@
 function calcularArea() {
-let B = parseFloat(document.getElementById("baseMayor").value);
-let b = parseFloat(document.getElementById("baseMenor").value);
-let h = parseFloat(document.getElementById("altura").value);
-let area = ((B + b) * h) / 2;
-document.getElementById("resultado").innerText = "Área: " + area.toFixed(2) + " m²";
+let A = parseFloat(document.getElementById("alturaTotal").value);
+let B = parseFloat(document.getElementById("base").value);
+let C = parseFloat(document.getElementById("alturaRect").value);
+
+if (C > A) {
+document.getElementById("resultado").innerText = "Error: C no puede ser mayor que A.";
+return;
+}
+
+let areaRect = B * C;
+let areaTriang = (B * (A - C)) / 2;
+let areaTotal = areaRect + areaTriang;
+
+document.getElementById("resultado").innerText = "Área total: " + areaTotal.toFixed(2) + " m²";
 }
